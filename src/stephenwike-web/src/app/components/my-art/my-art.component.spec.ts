857b49fcfe4905fe75cbb58f7eada0cbf9f752e0
Stephen Wike <stephen.c.wise@gmail.com>
2020-12-05 15:41:22 -0700

    cleaning again before angular cli update

--- src/stephenwike-web/src/app/components/my-art/my-art.component.spec.ts
+++ src/stephenwike-web/src/app/components/my-art/my-art.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { MyArtComponent } from './my-art.component';
 
@@ -6,7 +6,7 @@ describe('MyArtComponent', () => {
   let component: MyArtComponent;
   let fixture: ComponentFixture<MyArtComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ MyArtComponent ]
     })
--- src/stephenwike-web/src/app/open-close/open-close.component.spec.ts
+++ src/stephenwike-web/src/app/open-close/open-close.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { OpenCloseComponent } from './open-close.component';
 
@@ -6,7 +6,7 @@ describe('OpenCloseComponent', () => {
   let component: OpenCloseComponent;
   let fixture: ComponentFixture<OpenCloseComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ OpenCloseComponent ]
     })
